37f4a73fb7edf5895e246b5a6880fee549d3283a
petekozz <66498808+petekozz@users.noreply.github.com>
2020-10-28 19:35:08 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = 'skateboard'; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let facialHair = word.slice(5, 10);
+facialHair = facialHair.replace('o', 'e');
